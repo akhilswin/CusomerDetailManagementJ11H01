572af3b92375bfd9409281a27ffe19782d3b6b1e
Akhilesh <Akhilesh@Akhilesh-PC>
2020-09-08 16:55:43 +0530

    CusomerDetailManagementJ11H1

---
+++ CusomerDetailManagementJ11H1/src/test/java/yaksha/MainClassTest.java
@@ -0,0 +1,23 @@
+package yaksha;
+
+import static yaksha.TestUtils.businessTestFile;
+import static yaksha.TestUtils.currentTest;
+import static yaksha.TestUtils.yakshaAssert;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+class MainClassTest {
+	@Test
+	void testConvertCsvToMap() throws Exception {
+		// Test will pass
+		List<String> csvList = new ArrayList<String>();/////
+		String expectedResult[] = { "New York=1", "Texas=2" };
+		List<String> list1 = Arrays.asList(expectedResult);
+		Map<String, Integer> tm = CustomerDetailManagement.convertCsvToMap(csvList);
+		var list2 = Arrays.asList(tm);
+		yakshaAssert(currentTest(), (list1.containsAll(list2) ? "true" : "false"), businessTestFile);
+	}
+}
